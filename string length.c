e0c58d1aab540f8cf945cfeff132e5400cbf3067
Aditya Silswal <adiisilswal@gmail.com>
2024-11-24 20:26:54 +0530

    Create string length.c

---
+++ string length.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello, World!";  // Sample string
+
+    // Using strlen() to find the length of the string
+    int length = strlen(str);
+
+    // Displaying the result
+    printf("The length of the string is: %d\n", length);
+
+    return 0;
+}
